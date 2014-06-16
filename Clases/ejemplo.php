f21e025c14e8ff70bf4c2b2e6b4df2c009cb4647
Mark <mcmarco2@gmail.com>
2014-06-16 00:24:04 -0500

    Agregando material

---
+++ Clases/ejemplo.php
@@ -0,0 +1,7 @@
+<?php echo "Bienvenido a mi tienda online"; ?>
+
+<!-- tambien se puede mezclar html y php -->
+
+<h1><?php echo "Bienvenido a mi tienda online"; ?></h1>
+
+<p> La hora es: <?php echo date("H:i:s a"); ?></p>
